a4fd069457a19256bc31813112141f0498fe42fe
MaximeLesti <maxime.lestiboudois@heig-vd.ch>
2025-01-17 19:03:36 +0100

    premier jet

--- src/main/java/ch/heigvd/dai/controllers/SaleController.java
+++ src/main/java/ch/heigvd/dai/controllers/SaleController.java
@@ -1,5 +1,8 @@
 package ch.heigvd.dai.controllers;
 
+import ch.heigvd.dai.models.SaleRequest;
+import ch.heigvd.dai.models.SupplyRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.jasync.sql.db.Connection;
 import io.javalin.Javalin;
 import org.slf4j.Logger;
@@ -19,6 +22,24 @@ public class SaleController {
         app.post("/api/add-sale", ctx -> {
 
             logger.info("Requête reçue avec le corps suivant : {}", ctx.body());
+
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            SaleRequest request = objectMapper.readValue(ctx.body(), SaleRequest.class);
+
+            logger.info(request.toString());
+
+            logger.info("Données reçues : " + request);
+
+            try{
+
+
+
+            } catch (Exception e) {
+                logger.error("Erreur d'insertion :", e);
+                ctx.status(500).json(Map.of("error", "Erreur lors de l'enregistrement"));
+            }
+
 
             ctx.status(200).json(Map.of("message", "Succès"));
 
