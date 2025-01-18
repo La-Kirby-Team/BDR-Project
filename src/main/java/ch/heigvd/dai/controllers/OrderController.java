74ad44b3bbec12ba2639c4f70284128898fb350f
Bananonymous <leon.surbeck@gmail.com>
2025-01-18 18:45:21 +0100

    removed unused imports

--- src/main/java/ch/heigvd/dai/controllers/OrderController.java
+++ src/main/java/ch/heigvd/dai/controllers/OrderController.java
@@ -6,8 +6,6 @@ import com.github.jasync.sql.db.Connection;
 import com.github.jasync.sql.db.QueryResult;
 import com.github.jasync.sql.db.general.ArrayRowData;
 import io.javalin.Javalin;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 import java.util.Map;
--- src/main/java/ch/heigvd/dai/controllers/ProviderController.java
+++ src/main/java/ch/heigvd/dai/controllers/ProviderController.java
@@ -11,7 +11,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 import static ch.heigvd.dai.Main.logger;
 
